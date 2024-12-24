42c89abd52dffaeaa84f971e888f4bcf73ddc962
Aliyan Ali <darkpikayt1@gmail.com>
2024-12-24 09:40:23 +0500

    changes

--- calculator/app.js
+++ calculator/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const server = process.env.PORT || 5000;
-
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('prac');
